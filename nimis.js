130e8f070d3470dd4ffac79b49eb5ca79fc63ca4
Edin Jusupovic <edinjusupovic@hotmail.com>
2017-09-15 02:55:25 +0000

    all files now binded at the end, new readme updates, new general updates

--- nimis.js
+++ nimis.js
@@ -4,15 +4,21 @@ const zlib = require('zlib');
 
 let hash = require('./lib/hash_module.js').File; 
 let zip = require('./lib/zip_module.js').File;
-let uniqueFn = require('./lib/unique_module.js').c;
+let uniqueFn = require('./lib/unique_module.js').uniqueFileNameGeneration;
+let uniqueDir = require('./lib/unique_module.js').uniqueDirectoryNameGeneration;
 let fileStat = require('./lib/fileStatus_module.js').c; 
+let bindFiles = require('./lib/bind_module.js').c; 
 
-async function main(file){
+
+async function main(file, dir){
+    
     try {
         let hashInit = await hash(file);
         let uniqueFileName = await uniqueFn(); 
-            await zip(file, uniqueFileName);
-        let hashZip = await hash(`backups/${uniqueFileName}.gz`);
+            await zip(file, `${dir}/${uniqueFileName}.gz`);
+        
+        
+        let hashZip = await hash(`${dir}/${uniqueFileName}.gz`);
         
         return {
             initialNormalHash: hashInit, 
@@ -27,17 +33,21 @@ async function main(file){
 }
 
 
-exports.call = async function (arrayOfFiles){
+ exports.call = async function core(arrayOfFiles){
+     
     try{
         objectHolder = {};
-        
+        let tempDir = await uniqueDir(); 
+        let finalName = await uniqueFn();
         let initArray = arrayOfFiles; 
 
             for (let i=0; i<initArray.length;i++){
+                
                 let fileStatus = await fileStat(initArray[i]);
+                    
                 
                     if (fileStatus === 'OK'){
-                    let returnData = await main(initArray[i]);
+                    let returnData = await main(initArray[i], tempDir);
                     let currentPosition = `p${[i]}`;
              objectHolder[currentPosition] = {
              initHash: returnData.initialNormalHash,
@@ -53,11 +63,17 @@ exports.call = async function (arrayOfFiles){
         }            
     }
         
-     
+        await bindFiles(tempDir, `backups/${finalName}.gz`); // Combine all files
+        let bindZip = await hash(`backups/${finalName}.gz`);
+        
+        objectHolder['COMPLETE_GZIP'] = {
+             hash: bindZip,
+             fileName: `${finalName}.gz`
+        }
         return objectHolder // OBJECT RETURNED HERE  
+        
     } catch(err){
         throw new Error(err);
     }
 }
-
 
